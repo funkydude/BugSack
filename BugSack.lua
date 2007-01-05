fd7e309887aa3f675a3a9bc5ff7cb7e762ba6f48
Vidar Haarr <vhaarr@gmail.com>
2007-01-05 04:25:49 +0000

    BugSack: Relicensed as GPL v2 or later.

--- BugSack.lua
+++ BugSack.lua
@@ -7,6 +7,28 @@
 -- Credits to AceGUI & LuaPad for the scrollbar knowledge.
 --
 
+--[[
+
+BugSack, a World of Warcraft addon that interfaces with the !BugGrabber addon
+to present errors in a nice way.
+Copyright (C) 2007 The BugSack Team.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+]]
+
 local L = AceLibrary("AceLocale-2.2"):new("BugSack")
 BugSack = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceDB-2.0", "AceEvent-2.0")
 
