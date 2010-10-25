49804d6b39e63bcc266bf7c2765a4d64ef07f79f
Vidar Haarr <vhaarr@gmail.com>
2010-10-25 11:00:54 +0000

    Convert to using the localization system.

---
+++ locales.lua
@@ -0,0 +1,43 @@
+local AL = LibStub("AceLocale-3.0", true)
+if not AL then return end
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+local L = AL:NewLocale("BugSack", "enUS", true, debug)
+if L then
+--@localization(locale="enUS", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "deDE")
+if L then
+--@localization(locale="deDE", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "esES")
+if L then
+--@localization(locale="esES", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "esMX")
+if L then
+--@localization(locale="esMX", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "frFR")
+if L then
+--@localization(locale="frFR", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "koKR")
+if L then
+--@localization(locale="koKR", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "ruRU")
+if L then
+--@localization(locale="ruRU", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "zhCN")
+if L then
+--@localization(locale="zhCN", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+L = AL:NewLocale("BugSack", "zhTW")
+if L then
+--@localization(locale="zhTW", format="lua_additive_table", escape-non-ascii=true, same-key-is-true=true)@
+end
+
